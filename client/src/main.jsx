5b06c4696be0a9963cde8554ee7891ebcbc48b16
rooiss <louis.kim010@gmail.com>
2024-01-19 12:59:36 -0800

    added all mantine packages, postcss, and wrapped app in mantine provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import '@mantine/core/styles.css'
+
+import { MantineProvider } from '@mantine/core'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
 )
