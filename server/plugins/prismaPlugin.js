80748f6f4af3734dc1d39cd325d9d12396f43730
shihyulung <shihyulung@yahoo.com.tw>
2024-01-22 18:51:01 -0800

    run prisma in  container to receive database

---
+++ server/plugins/prismaPlugin.js
@@ -0,0 +1,17 @@
+import fp from 'fastify-plugin'
+import { PrismaClient } from '@prisma/client'
+
+const prismaPlugin = fp(async (server, options) => {
+  const prisma = new PrismaClient()
+
+  await prisma.$connect()
+
+  // Make Prisma Client available through the fastify server instance: server.prisma
+  server.decorate('prisma', prisma)
+
+  server.addHook('onClose', async (server) => {
+    await server.prisma.$disconnect()
+  })
+})
+
+export default prismaPlugin
