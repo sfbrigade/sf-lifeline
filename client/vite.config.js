f03f81f3754895be5a2948cc9577057f106bda12
samau3 <samau415@gmail.com>
2024-01-23 23:21:20 -0800

    Add server proxy configurations

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://0.0.0.0:5100",
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        }
+      }
+    }
+  }
 });
