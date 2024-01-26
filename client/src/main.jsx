d25d724cfb2fc3cdac9acdd2738b04c4a2d578a1
shihyulung <shihyulung@yahoo.com.tw>
2024-01-26 13:12:01 -0800

    incoporate mantine theme + dark/light mode to storybook interface

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import '@mantine/core/styles.css'
+import {theme} from './theme'
 
 import { MantineProvider } from '@mantine/core'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MantineProvider>
+    <MantineProvider theme={theme}>
       <App />
     </MantineProvider>
   </React.StrictMode>,
---
+++ client/src/theme.js
@@ -0,0 +1,7 @@
+import {createTheme} from "@mantine/core"
+
+export const theme = createTheme({
+//themes here 
+  fontFamily: 'serif'
+
+})
