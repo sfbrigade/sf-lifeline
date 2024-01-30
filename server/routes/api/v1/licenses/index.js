113defea0b37c8f836028555be7068ec60117c83
samau3 <samau415@gmail.com>
2024-01-30 14:18:16 -0800

    Add query string for license lookup

--- server/routes/api/v1/licenses/index.js
+++ server/routes/api/v1/licenses/index.js
@@ -4,8 +4,18 @@ import scrapeLicenses from "../../../../scraper/scraper.js";
 
 // User template for the routes
 export default async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    const results = await scrapeLicenses()
-    return results;
-  });
+  fastify.get(
+    '/',
+    {
+      schema: {
+        querystring: {
+          license: { type: 'string' }
+        }
+      }
+    },
+    async function (request, reply) {
+      const { license } = request.query
+      const results = await scrapeLicenses(license)
+      return results;
+    });
 }
