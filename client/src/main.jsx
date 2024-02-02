a8b99f6248548b1f47f3762619c938c55ad10bb8
Francis Li <mail@francisli.com>
2024-02-01 17:45:24 -0800

    Storybook init with latest version

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import '@mantine/core/styles.css';
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import '@mantine/core/styles.css'
-
-import { MantineProvider } from '@mantine/core'
+import { MantineProvider } from '@mantine/core';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
