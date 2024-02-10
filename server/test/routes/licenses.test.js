439171af704117e9867dfffac30e0094e3120762
samau3 <samau415@gmail.com>
2024-02-09 16:34:45 -0800

    Add basic test for license route

---
+++ server/test/routes/licenses.test.js
@@ -0,0 +1,26 @@
+import { test } from 'node:test'
+import * as assert from 'node:assert'
+import { build } from '../helper.js'
+
+
+test('License API returns valid results', async (t) => {
+  const app = await build(t)
+
+  const res = await app.inject({
+    url: '/api/v1/licenses?license=P39332'
+  })
+  const { name, licenseNumber } = JSON.parse(res.payload)
+  assert.equal(name, 'Koo, Chih Ren Nicholas')
+  assert.equal(licenseNumber, 'P39332')
+})
+
+test('License API returns error for invalid results', async (t) => {
+  const app = await build(t)
+
+  const res = await app.inject({
+    url: '/api/v1/licenses?license=1'
+  })
+  const { message } = JSON.parse(res.body)
+  assert.equal(res.statusCode, 500)
+  assert.equal(message, 'No match.')
+})
