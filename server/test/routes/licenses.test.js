4b8a4ec68f3b16b8e9105d93c187468f82943f8e
samau3 <samau415@gmail.com>
2024-02-15 17:00:05 -0800

    Switch to using describe and it to organize tests

--- server/test/routes/licenses.test.js
+++ server/test/routes/licenses.test.js
@@ -1,26 +1,33 @@
-import { test } from 'node:test';
+import { describe, it } from 'node:test';
 import * as assert from 'node:assert';
 import { build } from '../helper.js';
 
+describe('Testing License API route', () => {
+  it('should return valid results', async (t) => {
+    const app = await build(t);
 
-test('License API returns valid results', async (t) => {
-  const app = await build(t);
+    const validLicense = {
+      name: "Koo, Chih Ren Nicholas",
+      licenseType: "Paramedic",
+      status: "Active",
+      licenseNumber: "P39332"
+    }
 
-  const res = await app.inject({
-    url: '/api/v1/licenses?license=P39332'
+    const res = await app.inject({
+      url: '/api/v1/licenses?license=P39332'
+    });
+    assert.deepStrictEqual(JSON.parse(res.payload), validLicense)
+
   });
-  const { name, licenseNumber } = JSON.parse(res.payload);
-  assert.equal(name, 'Koo, Chih Ren Nicholas');
-  assert.equal(licenseNumber, 'P39332');
-})
 
-test('License API returns error for no matching results', async (t) => {
-  const app = await build(t);
+  it('should return a 404 error for no matching results', async (t) => {
+    const app = await build(t);
 
-  const res = await app.inject({
-    url: '/api/v1/licenses?license=1'
+    const res = await app.inject({
+      url: '/api/v1/licenses?license=1'
+    });
+    const { message } = JSON.parse(res.body);
+    assert.equal(res.statusCode, 404);
+    assert.equal(message, 'No match.');
   });
-  const { message } = JSON.parse(res.body);
-  assert.equal(res.statusCode, 404);
-  assert.equal(message, 'No match.');
-})
+});
