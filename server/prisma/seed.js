f18202e7669b9c54a7168134704e1ecb02e3ada5
Jackson Tran <69227799+javtran@users.noreply.github.com>
2024-03-07 15:49:30 -0800

    [Closes #23, Closes #24, Closes #25] Add Database Seeding Scripts (#47)
    * initial push
    
    * regex to parse medication name
    
    * added scripts to seed database
    
    * added category to conditions model
    
    * prisma database seeding
    
    * remove unneccessary packages
    
    * Configure server eslint settings for node instead of browser
    
    ---------
    
    Co-authored-by: Francis Li <mail@francisli.com>

---
+++ server/prisma/seed.js
@@ -0,0 +1,184 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+const MedicalSurgicalURL =
+  'https://nemsis.org/media/nemsis_v3/master/SuggestedLists/MedicalSurgicalHistory/MedicalSurgicalHistory.json';
+const MedicationAllergyURL =
+  'https://nemsis.org/media/nemsis_v3/master/SuggestedLists/MedicationAllergy/MedicationAllergy.json';
+const RxNormAPIEndpoint =
+  'https://rxnav.nlm.nih.gov/REST/allstatus.json?status=ACTIVE';
+const EnvFoodAllergyURL =
+  'https://nemsis.org/media/nemsis_v3/master/SuggestedLists/EnvironmentalFoodAllergy/EnvironmentalFoodAllergy.json';
+
+const CodingSystemEnum = {
+  SNOMED: 'SNOMED',
+  RXNORM: 'RXNORM',
+  ICD10: 'ICD10',
+};
+
+const AllergyType = {
+  DRUG: 'DRUG',
+  OTHER: 'OTHER',
+};
+
+async function seedConditions() {
+  try {
+    const response = await fetch(MedicalSurgicalURL);
+    const data = await response.json();
+    const codeList = data.DefinedList.Codes.Code;
+
+    const formattedCodesList = codeList.map((code) => {
+      return {
+        name: code.SuggestedLabel,
+        category: code.Category,
+        system: CodingSystemEnum.ICD10,
+        code: code.Value.Value,
+      };
+    });
+
+    await prisma.condition.createMany({
+      data: formattedCodesList,
+    });
+
+    console.log('Conditions seeded successfully');
+  } catch (error) {
+    console.error('Error seeding conditions:', error);
+  }
+}
+
+async function seedMedicationAllergies() {
+  try {
+    const response = await fetch(MedicationAllergyURL);
+    const data = await response.json();
+    const codeList = data.DefinedList.Codes.Code;
+
+    const formattedCodesList = codeList.map((code) => {
+      return {
+        name: code.SuggestedLabel,
+        type: AllergyType.DRUG,
+        system: CodingSystemEnum.ICD10,
+        code: code.Value.Value,
+      };
+    });
+
+    await prisma.allergy.createMany({
+      data: formattedCodesList,
+    });
+
+    console.log('Medication allergies seeded successfully');
+  } catch (error) {
+    console.error('Error seeding medication allergies:', error);
+  }
+}
+
+async function seedEnvFoodAllergies() {
+  try {
+    const response = await fetch(EnvFoodAllergyURL);
+    const data = await response.json();
+    const codeList = data.DefinedList.Codes.Code;
+
+    const formattedCodesList = codeList.map((code) => {
+      return {
+        name: code.SuggestedLabel,
+        type: AllergyType.OTHER,
+        system: CodingSystemEnum.SNOMED,
+        code: code.Value.Value,
+      };
+    });
+
+    await prisma.allergy.createMany({
+      data: formattedCodesList,
+    });
+
+    console.log('Env/Food allergies seeded successfully');
+  } catch (error) {
+    console.error('Error seeding env/food allergies:', error);
+  }
+}
+
+async function seedMedications() {
+  try {
+    const response = await fetch(RxNormAPIEndpoint);
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    const codeList = data.minConceptGroup.minConcept;
+
+    const packRegex = /\{([^}]+)\} Pack(?: \[([^\]]+)\])?/;
+    const nonPackRegex = /^(.*?)\s?(\[([^\]]+)\])?$/;
+
+    const formattedCodesList = codeList.map((code) => {
+      const packMatch = code.name.match(packRegex);
+      const nonPackMatch = code.name.match(nonPackRegex);
+
+      let name = '';
+      let alt = '';
+
+      if (packMatch) {
+        const medicineDetails = packMatch[1].trim();
+        const nameMatch = packMatch[2];
+
+        if (nameMatch) {
+          name = nameMatch.trim();
+          alt = medicineDetails;
+        } else {
+          name = medicineDetails;
+        }
+      } else if (nonPackMatch) {
+        const medicineDetails = nonPackMatch[1].trim();
+        const nameMatch = nonPackMatch[3] ? nonPackMatch[3].trim() : '';
+        if (nameMatch) {
+          name = nameMatch;
+          alt = medicineDetails;
+        } else {
+          name = medicineDetails;
+        }
+      } else {
+        console.log('no match: ', code.name);
+      }
+
+      return {
+        name: name,
+        altNames: alt,
+        system: CodingSystemEnum.RXNORM,
+        code: code.rxcui,
+      };
+    });
+
+    await prisma.medication.createMany({
+      data: formattedCodesList,
+    });
+    console.log('Medications seeded successfully');
+  } catch (error) {
+    console.error('Error seeding medications :', error);
+  }
+}
+
+async function main() {
+  // try {
+  //   await prisma.condition.deleteMany();
+  //   await prisma.allergy.deleteMany();
+  //   await prisma.medication.deleteMany();
+  //   console.log("clear db success");
+  // } catch (error) {
+  //   console.error('Error clearing tables:', error);
+  // }
+
+  await seedConditions();
+  await seedMedicationAllergies();
+  await seedEnvFoodAllergies();
+  await seedMedications();
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
