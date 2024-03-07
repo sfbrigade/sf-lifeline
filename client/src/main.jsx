9faaa8377b91ddde0cadca0fce27f4c5af7740c6
Drew Del Rosario <drewdelrosario@gmail.com>
2024-03-07 15:55:57 -0800

    [Closes #4] Add react-query package (#36)
    * Add react-query package
    
    * Update package-lock
    
    * Add react-query provider
    
    * Update providers
    
    * Remove unused variable
    
    * Clean up text
    
    ---------
    
    Co-authored-by: Francis Li <mail@francisli.com>

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.jsx';
 import './index.css';
 import '@mantine/core/styles.css';
@@ -7,10 +9,16 @@ import { theme } from './theme';
 
 import { MantineProvider } from '@mantine/core';
 
+const queryClient = new QueryClient({});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MantineProvider theme={theme}>
-      <App />
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <MantineProvider theme={theme}>
+          <App />
+        </MantineProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
