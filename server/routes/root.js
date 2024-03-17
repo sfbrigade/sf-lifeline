716be0515da864fdf176192056e1ead147448d96
Francis Li <mail@francisli.com>
2024-03-17 09:14:22 -0700

    Remove unnecessary route config

--- server/routes/root.js
+++ server/routes/root.js
@@ -2,6 +2,4 @@ export default async function (fastify, _opts) {
   fastify.get('/', async function (_request, _reply) {
     return { root: true };
   });
-
-  fastify.register(import('./api/v1/users/index.js'), { prefix: '/user' });
 }
