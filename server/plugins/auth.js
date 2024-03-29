2e9281f5b7da98507cadef62ee680cc609ece74f
Francis Li <mail@francisli.com>
2024-03-29 15:48:57 -0700

    More cleanup/naming tweaks

--- server/plugins/auth.js
+++ server/plugins/auth.js
@@ -3,7 +3,7 @@ import fp from 'fastify-plugin';
 // the use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
 
-export default fp(async function (fastify, _opts) {
+export default fp(async function (fastify) {
   // set up secure encrypted cookie-based sessions
   await fastify.register(import('@fastify/secure-session'), {
     key: Buffer.from(process.env.SESSION_SECRET_KEY, 'hex'),
@@ -16,7 +16,7 @@ export default fp(async function (fastify, _opts) {
   // add a user object reference to the request instance
   fastify.decorateRequest('user', null);
   // add a hook to check for a signed in user on every request
-  fastify.addHook('onRequest', async (request, _reply) => {
+  fastify.addHook('onRequest', async (request) => {
     // first check cookie-based session
     const id = request.session.get('userId');
     if (id) {
