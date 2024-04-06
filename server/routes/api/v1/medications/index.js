4b4c3328a5671a763e281f9f1355cb3e9549033e
samau3 <samau415@gmail.com>
2024-04-05 18:31:32 -0700

    Add edge cases returns for medications

--- server/routes/api/v1/medications/index.js
+++ server/routes/api/v1/medications/index.js
@@ -9,7 +9,18 @@ export default async function (fastify) {
       },
     },
     async (request, reply) => {
-      const { medication } = request.query;
+      const  medication = request.query.medication.trim();
+
+      if (!medication.length) {
+        return
+      }
+
+      const NO_MEDICATIONS = ['none', 'no medications', 'no known medications', 'n/a'];
+
+      if (NO_MEDICATIONS.includes(medication.toLowerCase())){
+        return reply.send('No known medications');
+      }
+
       const results = await fastify.prisma.medication.findMany({
         orderBy: [{ name: 'asc' }],
         where: { name: { contains: medication, mode: 'insensitive' } },
