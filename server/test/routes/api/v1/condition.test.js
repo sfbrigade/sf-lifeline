2f1de09f8748a378a5e713e49cd2e83136cbcb25
samau3 <samau415@gmail.com>
2024-04-06 00:12:32 -0700

    Add StatusCodes import

--- server/test/routes/api/v1/condition.test.js
+++ server/test/routes/api/v1/condition.test.js
@@ -1,6 +1,7 @@
 import { describe, it } from 'node:test';
 import * as assert from 'node:assert';
 import { build } from '../../../helper.js';
+import { StatusCodes } from 'http-status-codes';
 
 describe('/api/v1/conditions', () => {
   describe('GET /', () => {
