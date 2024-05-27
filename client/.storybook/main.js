8ab40f69a64e7cba24c18748ae30fb90e8e53e28
chengtchris1 <152359315+chengtchris1@users.noreply.github.com>
2024-05-27 16:50:07 -0700

    Upgraded Storybook to 8.1.4, the latest version by running npx storybook@latest upgrade

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -11,6 +11,7 @@ function getAbsolutePath(value) {
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
   addons: [
     getAbsolutePath('@storybook/addon-links'),
     getAbsolutePath('@storybook/addon-essentials'),
@@ -18,13 +19,19 @@ const config = {
     getAbsolutePath('@storybook/addon-interactions'),
     getAbsolutePath('storybook-dark-mode'),
     getAbsolutePath('@storybook/addon-mdx-gfm'),
+    getAbsolutePath("@storybook/addon-mdx-gfm"),
+    '@chromatic-com/storybook'
   ],
+
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
-  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 };
 export default config;
