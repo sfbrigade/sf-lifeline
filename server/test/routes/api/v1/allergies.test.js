20a94964f094383b787deaa8f00cf55a2e319c56
Sammy Au <69769431+samau3@users.noreply.github.com>
2024-07-24 11:46:08 -0700

    [Closes #45] Admin/Staff/Volunteer can search allergy/medication/condition data (#70)
    * Basic route for querying allergies started"
    
    * Switch to using contains instead of starts with
    
    * Basic working medication search route
    
    * Rename folder to follow naming convention
    
    * Remove returning other when entering an unknown allergy
    
    * Add route for conditions table search
    
    * Add edge cases for no entry and variations of no allergies
    
    * Add edge case returns for conditions
    
    * Add edge cases returns for medications
    
    * Format files
    
    * Reduce the field returned to just be name
    
    * Add allergy table fixture for testing
    
    * Maintain similar return data structure type from route
    
    * Add tests for allergy route
    
    * Differentiate between no query and no db results for allergies
    
    * Update routes to differentiate between empty query and no db results
    
    * Update allergies test descriptions
    
    * Add tests for conditions route
    
    * Add tests for medications route
    
    * Use not found instead of a message for queries not found in db
    
    * Remove invalid field for medication table
    
    * Use message instead of name as the key when returning a message
    
    * Update tests to match changes in returned values and use StatusCodes
    
    * Add StatusCodes import
    
    * Add concurrency to test sript to prevent db issues
    
    * Implement the WIP helpers and structure from PR #69
    
    * Add ability for requireUser to accept multiple roles
    
    * Add additional users for staff and volunteer roles
    
    * Update routes and tests to utilize headers
    
    * Format file
    
    * Remove unused t variable to resolve lint errors
    
    * Return an empty array when query not in found in db
    
    * Remove n/a input handling as it should be handled elsewhere
    
    * Update routes to paginate and return count of total results
    
    * Update tests to reflect changes in routes
    
    * Pattern match users list.js to utilize existing helper functions
    
    * Add back trimming of query parameter
    
    * Pattern match applying existing helper functions to remaining routes
    
    * Update fixtures to include id field
    
    * Update tests to reflect changes in routes
    
    * Add StatusCodes import
    
    ---------
    
    Co-authored-by: Francis Li <mail@francisli.com>

---
+++ server/test/routes/api/v1/allergies.test.js
@@ -0,0 +1,136 @@
+import { beforeEach, describe, it } from 'node:test';
+import * as assert from 'node:assert';
+import { build } from '../../../helper.js';
+import { StatusCodes } from 'http-status-codes';
+
+describe('/api/v1/allergies', () => {
+  let app;
+  let headers;
+
+  beforeEach(async (t) => {
+    app = await build(t);
+    await t.loadFixtures();
+    headers = await t.authenticate('admin.user@test.com', 'test');
+  });
+
+  describe('GET /', () => {
+    it('should return valid results for admin user', async () => {
+      const reply = await app
+        .inject()
+        .get('/api/v1/allergies?allergy=p&perPage=1')
+        .headers(headers);
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
+      console.log("GET / Headers" ,reply.payload)
+      assert.deepStrictEqual(
+        reply.headers['link'],
+        '<http://localhost/api/v1/allergies?allergy=p&perPage=1&page=2>; rel="next"',
+      );
+      assert.deepStrictEqual(JSON.parse(reply.payload),
+        [{
+          id: "ceb1cd02-d5a7-46ef-915f-766cee886d0d",
+          name: "Grass Pollen",
+          type: "OTHER",
+          system: "SNOMED",
+          code: "418689008"
+        }]
+      );
+    });
+
+    it('should return valid results for staff user', async (t) => {
+      const staffHeaders = await t.authenticate('staff.user@test.com', 'test');
+
+      const reply = await app
+        .inject()
+        .get('/api/v1/allergies?allergy=p&perPage=1')
+        .headers(staffHeaders);
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
+      assert.deepStrictEqual(
+        reply.headers['link'],
+        '<http://localhost/api/v1/allergies?allergy=p&perPage=1&page=2>; rel="next"',
+      );
+      assert.deepStrictEqual(JSON.parse(reply.payload),
+        [{
+          id: "ceb1cd02-d5a7-46ef-915f-766cee886d0d",
+          name: "Grass Pollen",
+          type: "OTHER",
+          system: "SNOMED",
+          code: "418689008"
+        }]
+      );
+    });
+
+    it('should return valid results for volunteer user', async (t) => {
+      const volunteerHeaders = await t.authenticate('volunteer.user@test.com', 'test');
+
+      const reply = await app
+        .inject()
+        .get('/api/v1/allergies?allergy=p&perPage=1')
+        .headers(volunteerHeaders);
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
+      assert.deepStrictEqual(
+        reply.headers['link'],
+        '<http://localhost/api/v1/allergies?allergy=p&perPage=1&page=2>; rel="next"',
+      );
+      assert.deepStrictEqual(JSON.parse(reply.payload),
+        [{
+          id: "ceb1cd02-d5a7-46ef-915f-766cee886d0d",
+          name: "Grass Pollen",
+          type: "OTHER",
+          system: "SNOMED",
+          code: "418689008"
+        }]
+      );
+    });
+
+    it('require a user to be admin/staff/volunteer to make requests', async () => {
+
+      const reply = await app
+        .inject()
+        .get('/api/v1/allergies?allergy=p&perPage=1')
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.UNAUTHORIZED);
+    });
+
+    it('should return paginated results of all allergies when no query provided', async () => {
+
+      const reply = await app
+        .inject()
+        .get('/api/v1/allergies?allergy=&perPage=1')
+        .headers(headers);
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
+      assert.deepStrictEqual(
+        reply.headers['link'],
+        '<http://localhost/api/v1/allergies?allergy=&perPage=1&page=2>; rel="next",<http://localhost/api/v1/allergies?allergy=&perPage=1&page=3>; rel="last"',
+      );
+      assert.deepStrictEqual(JSON.parse(reply.payload),
+        [{
+          id: "ceb1cd02-d5a7-46ef-915f-766cee886d0d",
+          name: "Grass Pollen",
+          type: "OTHER",
+          system: "SNOMED",
+          code: "418689008"
+        }]
+      );
+    });
+
+    it('should return no results from database an unknown allergy', async () => {
+
+      const reply = await app
+        .inject()
+        .get('/api/v1/allergies?allergy=newallergy&perPage=1')
+        .headers(headers);
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
+      assert.deepStrictEqual(
+        reply.headers['link'],
+        '',
+      );
+      assert.deepStrictEqual(JSON.parse(reply.payload), []);
+    });
+
+  });
+});
---
+++ server/test/routes/api/v1/medications.test.js
@@ -0,0 +1,101 @@
+import { beforeEach, describe, it } from 'node:test';
+import * as assert from 'node:assert';
+import { build } from '../../../helper.js';
+import { StatusCodes } from 'http-status-codes';
+
+describe('/api/v1/medications', () => {
+  let app;
+  let headers;
+
+  beforeEach(async (t) => {
+    app = await build(t);
+    await t.loadFixtures();
+    headers = await t.authenticate('admin.user@test.com', 'test');
+  });
+
+  describe('GET /', () => {
+    it('should return valid results for admin user', async () => {
+      const reply = await app
+        .inject()
+        .get('/api/v1/medications?medication=ibuprofen&perPage=1')
+        .headers(headers);
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
+      assert.deepStrictEqual(
+        reply.headers['link'],
+        '<http://localhost/api/v1/medications?medication=ibuprofen&perPage=1&page=2>; rel="next"',
+      );
+      assert.deepStrictEqual(JSON.parse(reply.payload), [{ id: '583c7775-9466-4dab-8a4d-edf1056f097f', name: 'acetaminophen / ibuprofen', altNames: '', system: 'RXNORM', code: '818102' }]);
+    });
+
+    it('should return valid results for staff user', async (t) => {
+      const staffHeaders = await t.authenticate('staff.user@test.com', 'test');
+
+      const reply = await app
+        .inject()
+        .get('/api/v1/medications?medication=ibuprofen&perPage=1')
+        .headers(staffHeaders);
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
+      assert.deepStrictEqual(
+        reply.headers['link'],
+        '<http://localhost/api/v1/medications?medication=ibuprofen&perPage=1&page=2>; rel="next"',
+      );
+      assert.deepStrictEqual(JSON.parse(reply.payload), [{ id: '583c7775-9466-4dab-8a4d-edf1056f097f', name: 'acetaminophen / ibuprofen', altNames: '', system: 'RXNORM', code: '818102' }]);
+    });
+
+    it('should return valid results for volunteer user', async (t) => {
+      const volunteerHeaders = await t.authenticate('volunteer.user@test.com', 'test');
+
+      const reply = await app
+        .inject()
+        .get('/api/v1/medications?medication=ibuprofen&perPage=1')
+        .headers(volunteerHeaders);
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
+      assert.deepStrictEqual(
+        reply.headers['link'],
+        '<http://localhost/api/v1/medications?medication=ibuprofen&perPage=1&page=2>; rel="next"',
+      );
+      assert.deepStrictEqual(JSON.parse(reply.payload), [{ id: '583c7775-9466-4dab-8a4d-edf1056f097f', name: 'acetaminophen / ibuprofen', altNames: '', system: 'RXNORM', code: '818102' }]);
+    });
+
+    it('require a user to be admin/staff/volunteer to make requests', async () => {
+
+      const reply = await app
+        .inject()
+        .get('/api/v1/medications?medication=ibuprofen&perPage=1')
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.UNAUTHORIZED);
+    });
+
+    it('should return paginated results of all medications when no query provided', async () => {
+      const reply = await app
+        .inject()
+        .get('/api/v1/medications?medication=&perPage=1')
+        .headers(headers);
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
+      assert.deepStrictEqual(
+        reply.headers['link'],
+        '<http://localhost/api/v1/medications?medication=&perPage=1&page=2>; rel="next",<http://localhost/api/v1/medications?medication=&perPage=1&page=3>; rel="last"',
+      );
+      assert.deepStrictEqual(JSON.parse(reply.payload), [{ id: '583c7775-9466-4dab-8a4d-edf1056f097f', name: 'acetaminophen / ibuprofen', altNames: '', system: 'RXNORM', code: '818102' }]);
+    });
+
+    it('should return no results from database for an unknown medication', async () => {
+      const reply = await app
+        .inject()
+        .get('/api/v1/medications?medication=newmedication&perPage=1')
+        .headers(headers);
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
+      assert.deepStrictEqual(
+        reply.headers['link'],
+        '',
+      );
+      assert.deepStrictEqual(JSON.parse(reply.payload), []);
+    });
+
+  });
+});
