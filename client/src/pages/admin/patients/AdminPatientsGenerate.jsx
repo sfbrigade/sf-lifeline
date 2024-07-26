769c2010bdcbd2529ca6702d477f5eac180e387b
Francis Li <mail@francisli.com>
2024-07-25 18:51:47 -0700

    Fix formatting

--- client/src/pages/admin/patients/AdminPatientsGenerate.jsx
+++ client/src/pages/admin/patients/AdminPatientsGenerate.jsx
@@ -4,7 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 import { QRCode } from 'react-qrcode-logo';
 
 /**
- * 
+ *
  * @returns {React.ReactElement}
  */
 function AdminPatientsGenerate() {
