111f812232787fa48f31c3619faaefa60e7c667c
samau3 <samau415@gmail.com>
2024-07-31 20:50:04 -0700

    Wrap patient registration in a db transaction

--- server/routes/api/v1/patients/register.js
+++ server/routes/api/v1/patients/register.js
@@ -8,12 +8,28 @@ export default async function (fastify, _opts) {
       schema: {
         body: {
           type: 'object',
-          required: ['firstName', 'lastName', 'dateOfBirth'],
           properties: {
-            firstName: { type: 'string' },
-            middleName: { type: 'string' },
-            lastName: { type: 'string' },
-            dateOfBirth: { type: 'string'},
+            patientData: {
+              type: 'object',
+              required: ['firstName', 'lastName', 'dateOfBirth'],
+              properties: {
+                firstName: { type: 'string' },
+                middleName: { type: 'string' },
+                lastName: { type: 'string' },
+                dateOfBirth: { type: 'string' },
+              },
+            },
+            contactData: {
+              type: 'object',
+              required: ['firstName', 'lastName', 'phone', 'relationship'],
+              properties: {
+                firstName: { type: 'string' },
+                middleName: { type: 'string' },
+                lastName: { type: 'string' },
+                phone: { type: 'string' },
+                relationship: { type: 'string' },
+              },
+            },
           },
         },
         response: {
@@ -28,31 +44,44 @@ export default async function (fastify, _opts) {
       onRequest: fastify.requireUser([Role.ADMIN, Role.STAFF, Role.VOLUNTEER]),
     },
     async (request, reply) => {
-      const {
-        firstName,
-        middleName,
-        lastName,
-        dateOfBirth,
-      } = request.body;
+      const { patientData, contactData } = request.body;
 
-      const errorList = new Array();
-
-      
       const userId = request.user.id;
 
-      let patient = await fastify.prisma.patient.create({
-        data: {
-          firstName,
-          middleName,
-          lastName,
-          dateOfBirth,
-          createdById: userId,
-          updatedById: userId,
-        }
-      })
+      const newPatient = await fastify.prisma.$transaction(async (tx) => {
+        let patient = await tx.patient.create({
+          data: {
+            firstName: patientData.firstName,
+            middleName: patientData.middleName,
+            lastName: patientData.lastName,
+            dateOfBirth: patientData.dateOfBirth,
+            createdById: userId,
+            updatedById: userId,
+          },
+        });
 
+        let contact = await tx.contact.create({
+          data: {
+            firstName: contactData.firstName,
+            middleName: contactData.middleName,
+            lastName: contactData.lastName,
+            phone: contactData.phone,
+            relationship: contactData.relationship,
+          },
+        });
 
-      reply.code(StatusCodes.CREATED).send(patient.id);
+        await tx.patient.update({
+          where: {
+            id: patient.id,
+          },
+          data: {
+            emergencyContactId: contact.id,
+          },
+        });
+        return patient.id;
+      });
+
+      reply.code(StatusCodes.CREATED).send(newPatient);
     },
   );
 }
