f9b826edec3284fa6f77a984c90d00208c96024c
Jackson Tran <javtran@ucsc.edu>
2024-08-04 23:33:15 -0700

    styled modal

--- client/src/pages/admin/users/InviteModal.jsx
+++ client/src/pages/admin/users/InviteModal.jsx
@@ -1,18 +1,67 @@
 import React from 'react';
-import { Modal } from '@mantine/core';
+import { Modal, Select, TextInput, Group, Button } from '@mantine/core';
+import { useForm } from '@mantine/form';
 import PropTypes from 'prop-types';
 import classes from './admin.module.css';
 
 const inviteModalProps = {
-    opened: PropTypes.bool.isRequired,
-    close: PropTypes.func.isRequired,
-}
-export function InviteModal ({
-    opened,
-    close
-}) {
-    const title = <div><div className='title'>Single Invite</div><p>Add your documents here, and you can upload 1 file max</p></div>;
-    return <Modal opened={opened} onClose={close} title={title} className={classes.modal}>ASDJFOAJSEOFJAOSEJFOAJSEOF</Modal>
+  opened: PropTypes.bool.isRequired,
+  close: PropTypes.func.isRequired,
+};
+export function InviteModal({ opened, close }) {
+  const form = useForm({
+    mode: 'uncontrolled',
+    initialValues: {
+      role: 'Volunteer',
+      name: '',
+      email: '',
+    },
+  });
+
+  const title = (
+    <div>
+      <h3>Single Invite</h3>
+      <p>Select the role and enter the recepient's name and email</p>
+    </div>
+  );
+  return (
+    <Modal
+      opened={opened}
+      onClose={close}
+      title={title}
+      className={classes.modal}
+    >
+      <form
+        className="form"
+        onSubmit={form.onSubmit((values) => console.log(values))}
+      >
+        <Select
+          label="Member Type"
+          data={['Volunteer', 'First Responder', 'Admin']}
+          key={form.key('role')}
+          {...form.getInputProps('role')}
+        />
+        <TextInput
+          label="Name"
+          placeholder="Name"
+          key={form.key('name')}
+          {...form.getInputProps('name')}
+        />
+        <TextInput
+          label="Email"
+          placeholder="Email"
+          key={form.key('email')}
+          {...form.getInputProps('email')}
+        />
+        <Group justify="flex-end">
+          <Button variant="outline" color="gray" onClick={close}>
+            Cancel
+          </Button>
+          <Button type="submit">Send Invite</Button>
+        </Group>
+      </form>
+    </Modal>
+  );
 }
 
 InviteModal.propTypes = inviteModalProps;
