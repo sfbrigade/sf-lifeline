0a8c9f34e0eccb3ab6c4a45714517246e3d03f00
Jackson Tran <javtran@ucsc.edu>
2024-08-27 22:27:15 -0700

    fix auth page layout and css

--- client/src/stories/AuthLayout/AuthLayout.jsx
+++ client/src/stories/AuthLayout/AuthLayout.jsx
@@ -9,7 +9,9 @@ export default function AuthLayout() {
   return (
     <div className="auth-layout">
       <div className="banner">testing</div>
-      <Outlet />
+      <div className="form">
+        <Outlet />
+      </div>
     </div>
   );
 }
