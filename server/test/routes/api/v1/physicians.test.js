29b581e4329906d41fe4c48aa9bd7b331f53ddcc
samau3 <samau415@gmail.com>
2024-09-04 21:57:01 -0700

    Update tests

--- server/test/routes/api/v1/physicians.test.js
+++ server/test/routes/api/v1/physicians.test.js
@@ -4,76 +4,76 @@ import { StatusCodes } from 'http-status-codes';
 
 import { build } from '../../../helper.js';
 
-describe('/api/v1/hospitals', () => {
+describe('/api/v1/physicians', () => {
   describe('GET /', () => {
-    it('should return all hospitals for ADMIN user', async (t) => {
+    it('should return all physicians for ADMIN user', async (t) => {
       const app = await build(t);
       await t.loadFixtures();
       const headers = await t.authenticate('admin.user@test.com', 'test');
       const reply = await app
         .inject()
-        .get('/api/v1/hospitals?hospital=&perPage=1')
+        .get('/api/v1/physicians?physician=&perPage=1')
         .headers(headers);
 
       assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
       assert.deepStrictEqual(
         reply.headers['link'],
-        '<http://localhost/api/v1/hospitals?hospital=&perPage=1&page=2>; rel="next",<http://localhost/api/v1/hospitals?hospital=&perPage=1&page=3>; rel="last"',
+        '<http://localhost/api/v1/physicians?physician=&perPage=1&page=2>; rel="next",<http://localhost/api/v1/physicians?physician=&perPage=1&page=3>; rel="last"',
       );
       assert.deepStrictEqual(JSON.parse(reply.payload).length, 1);
     });
 
-    it('should return hospitals for STAFF user', async (t) => {
+    it('should return physicians for STAFF user', async (t) => {
       const app = await build(t);
       await t.loadFixtures();
       const headers = await t.authenticate('staff.user@test.com', 'test');
       const reply = await app
         .inject()
-        .get('/api/v1/hospitals?hospital=')
+        .get('/api/v1/physicians?physician=')
         .headers(headers);
 
       assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
       assert.deepStrictEqual(JSON.parse(reply.payload).length, 3);
     });
 
-    it('should return hospitals for VOLUNTEER user', async (t) => {
+    it('should return physicians for VOLUNTEER user', async (t) => {
       const app = await build(t);
       await t.loadFixtures();
       const headers = await t.authenticate('volunteer.user@test.com', 'test');
       const reply = await app
         .inject()
-        .get('/api/v1/hospitals?hospital=')
+        .get('/api/v1/physicians?physician=')
         .headers(headers);
 
       assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
       assert.deepStrictEqual(JSON.parse(reply.payload).length, 3);
     });
 
-    it('should return no hospitals for FIRST_RESPONDER user', async (t) => {
+    it('should return no physicians for FIRST_RESPONDER user', async (t) => {
       const app = await build(t);
       await t.loadFixtures();
       const headers = await t.authenticate('first.responder@test.com', 'test');
       const reply = await app
         .inject()
-        .get('/api/v1/hospitals?hospital=')
+        .get('/api/v1/physicians?physician=')
         .headers(headers);
 
       assert.deepStrictEqual(reply.statusCode, StatusCodes.FORBIDDEN);
     });
 
-    it('should return a hospitals for ADMIN user', async (t) => {
+    it('should return a physicians for ADMIN user', async (t) => {
       const app = await build(t);
       await t.loadFixtures();
       const headers = await t.authenticate('admin.user@test.com', 'test');
       const reply = await app
         .inject()
-        .get('/api/v1/hospitals?hospital=SF')
+        .get('/api/v1/physicians?physician=smith')
         .headers(headers);
 
       assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
       assert.deepStrictEqual(JSON.parse(reply.payload).length, 2);
-      assert.deepStrictEqual(JSON.parse(reply.payload)[0].name, 'Kaiser SF');
-      assert.deepStrictEqual(JSON.parse(reply.payload)[1].name, 'SF General');
+      assert.deepStrictEqual(JSON.parse(reply.payload)[0].firstName, 'Bob');
+      assert.deepStrictEqual(JSON.parse(reply.payload)[1].firstName, 'John');
     });
   });
 });
