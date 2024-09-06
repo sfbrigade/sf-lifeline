7658273d94a95a159db3d12a5825f89fba32c686
Francis Li <mail@francisli.com>
2024-09-06 15:51:13 -0700

    Email tweaks

--- server/helpers/email/mailer.js
+++ server/helpers/email/mailer.js
@@ -53,6 +53,15 @@ const mailer = new Email({
       extension: 'ejs',
     },
   },
+  juice: false,
 });
 
-export default mailer;
+function send(options) {
+  options.locals ||= {};
+  options.locals._layout = {
+    logoUrl: `${process.env.BASE_URL}/logo.svg`,
+  };
+  mailer.send(options);
+}
+
+export default { send };
