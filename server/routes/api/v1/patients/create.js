338c7725905bdbf531cc9274f7c776163c2fa7e0
samau3 <samau415@gmail.com>
2024-09-11 18:36:21 -0700

    Properly handle throwing a duplicate patient error

--- server/routes/api/v1/patients/create.js
+++ server/routes/api/v1/patients/create.js
@@ -1,6 +1,8 @@
 import { Role } from '../../../../models/user.js';
 import { StatusCodes } from 'http-status-codes';
 
+
+
 export default async function (fastify, _opts) {
   fastify.post(
     '/',
@@ -67,39 +69,49 @@ export default async function (fastify, _opts) {
       const { id } = request.body;
 
       const userId = request.user.id;
+      try {
+        const newPatient = await fastify.prisma.$transaction(async (tx) => {
+          // Check if the patient already exists
+          const exists = await tx.patient.findUnique({
+            where: { id },
+          });
+          if (exists) {
+            throw new Error(
+              
+              `Patient with ID ${id} already exists in database.`,
+            );
+          }
 
-      const newPatient = await fastify.prisma.$transaction(async (tx) => {
-        // Check if the patient already exists
-        const exists = await tx.patient.findUnique({
-          where: { id },
-        });
-        if (exists)
-          throw reply.status(StatusCodes.CONFLICT).send({
-            message: `Patient with ID ${id} already exists in database.`,
+          const newPatientData = {};
+
+          for (const [key, value] of Object.entries(request.body)) {
+            if (value) newPatientData[key] = value.trim();
+            if (key === 'middleName' && value.length === 0) {
+              newPatientData[key] = null;
+            }
+            if (key === 'dateOfBirth') newPatientData[key] = new Date(value);
+          }
+
+          let patient = await tx.patient.create({
+            data: {
+              ...newPatientData,
+              createdById: userId,
+              updatedById: userId,
+            },
           });
 
-        const newPatientData = {};
-
-        for (const [key, value] of Object.entries(request.body)) {
-          if (value) newPatientData[key] = value.trim();
-          if (key === 'middleName' && value.length === 0) {
-            newPatientData[key] = null;
-          }
-          if (key === 'dateOfBirth') newPatientData[key] = new Date(value);
-        }
-
-        let patient = await tx.patient.create({
-          data: {
-            ...newPatientData,
-            createdById: userId,
-            updatedById: userId,
-          },
+          return patient;
         });
 
-        return patient;
-      });
-
-      reply.code(StatusCodes.CREATED).send(newPatient);
+        reply.code(StatusCodes.CREATED).send(newPatient);
+      } catch (error) {
+        if (error.message.includes('already exists')) {
+          return reply.status(StatusCodes.CONFLICT).send({
+            message: error.message,
+          });
+        }
+        throw error;
+      }
     },
   );
 }
