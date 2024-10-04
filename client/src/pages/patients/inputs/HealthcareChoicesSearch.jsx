a03668831ef5836395f46031873d29ff7dbe4e96
Wendy <wendy@Wendys-MacBook-Air.local>
2024-10-04 12:48:46 -0700

    Displays physician followed by hospital name only if it exists

--- client/src/pages/patients/inputs/HealthcareChoicesSearch.jsx
+++ client/src/pages/patients/inputs/HealthcareChoicesSearch.jsx
@@ -91,7 +91,7 @@ export default function HealthcareChoicesSearch({ form, choice, initialData }) {
 
   const options = (data || []).map((item) => (
     <Combobox.Option value={item.id} key={item.id}>
-      {item.name} - {item.hospitals[0]?.name}
+      {item.name} {item.hospitals?.length && `- ${item.hospitals[0].name}`}
     </Combobox.Option>
   ));
 
