03dec8b82d0497a0e114d850251cd496007bee1f
Sammy Au <69769431+samau3@users.noreply.github.com>
2024-10-10 15:07:24 -0700

    [Closes #114, #122] Add checkmark to viewed patient registration form sections (#128)
    * Restyle Accordion
    
    * Add a check mark component to be conditionally rendered
    
    * Add logic to conditionally render accordion checkmarks
    
    * Remove unused code
    
    Removed the logic that prevented users from going to prev form sections if there were form errors
    
    * Add scrollbar gutter to avoid layout shift when scrollbar appears
    
    * Remove scrollbar gutter styling
    
    * Increase space between checkmark and chevron on accordion
    
    * Reword hospital input label to be more clear about what it represents
    
    * Update check states only when field data are successfully sent to server
    
    * Reduce prop drilling and unnecessary complexity
    
    Resolved bug when trying to change prefilled healthcare choices due to improper deletion logic
    Removed unnecessary inputValue prop since it was duplicative of searchQuery
    
    * Clean up dead code
    
    * Convert enum values of form fields to be more readable
    
    * Format files

--- client/src/pages/patients/PatientRegistration.jsx
+++ client/src/pages/patients/PatientRegistration.jsx
@@ -10,15 +10,7 @@ import { useQuery } from '@tanstack/react-query';
 import LifelineAPI from './LifelineAPI.js';
 import PatientRegistrationAccordion from './PatientRegistrationAccordion';
 
-const TABS = [
-  'patientData',
-  'contactData',
-  'medicalData',
-  'healthcareChoices',
-  'codeStatus',
-];
-
-const ERROR_SECTION_MAP = {
+const FORM_TABS = {
   patientData: 'Patient Data',
   contactData: 'Emergency Contact',
   medicalData: 'Medical Information',
@@ -32,12 +24,19 @@ const ERROR_SECTION_MAP = {
  */
 export default function PatientRegistration() {
   const [loading, setLoading] = useState(false);
-  const [active, setActive] = useState(0);
   const [initialMedicalData, setInitialMedicalData] = useState({});
-  const [initialHospitalData, setInitialHospitalData] = useState({});
-  const [initialPhysicianData, setInitialPhysicianData] = useState({});
+  const [initialHospitalData, setInitialHospitalData] = useState('');
+  const [initialPhysicianData, setInitialPhysicianData] = useState('');
   const [openedSection, setOpenedSection] = useState('patientData');
   const [opened, { open, close }] = useDisclosure(false);
+
+  const [showCheck, setShowCheck] = useState({
+    patientData: false,
+    contactData: false,
+    medicalData: false,
+    healthcareChoices: false,
+    codeStatus: false,
+  });
 
   const [visitedSections, setVisitedSections] = useState({
     patientData: true,
@@ -168,15 +167,11 @@ export default function PatientRegistration() {
         }),
       });
 
-      setInitialHospitalData({
-        id: hospital ? hospital.id : '',
-        name: hospital ? hospital.name : '',
-      });
+      setInitialHospitalData(hospital ? hospital.name : '');
 
-      setInitialPhysicianData({
-        id: physician ? physician.id : '',
-        name: physician ? `${physician.firstName} ${physician.lastName}` : '',
-      });
+      setInitialPhysicianData(
+        physician ? `${physician.firstName} ${physician.lastName}` : '',
+      );
 
       const patientData = {
         firstName,
@@ -251,7 +246,7 @@ export default function PatientRegistration() {
 
     const unvisited = Object.entries(visitedSections)
       .filter(([_, visited]) => !visited)
-      .map(([section]) => ERROR_SECTION_MAP[section]);
+      .map(([section]) => FORM_TABS[section]);
 
     if (unvisited.length > 0) {
       setUnvisitedSections(unvisited);
@@ -324,7 +319,7 @@ export default function PatientRegistration() {
 
     let errorSections = [];
     errorSets.forEach((key) => {
-      errorSections.push(ERROR_SECTION_MAP[key]);
+      errorSections.push(FORM_TABS[key]);
     });
 
     showErrorNotification(
@@ -343,6 +338,10 @@ export default function PatientRegistration() {
         showSuccessNotification(
           'Patient basic information has been successfully registered.',
         );
+        setShowCheck((prevShowCheck) => ({
+          ...prevShowCheck,
+          patientData: true,
+        }));
         return;
       }
 
@@ -355,6 +354,10 @@ export default function PatientRegistration() {
           showSuccessNotification(
             'Patient basic information has been successfully updated.',
           );
+          setShowCheck((prevShowCheck) => ({
+            ...prevShowCheck,
+            patientData: true,
+          }));
           return;
         }
         throw new Error('Failed to update patient.');
@@ -382,6 +385,10 @@ export default function PatientRegistration() {
         showSuccessNotification(
           'Patient information has been successfully updated.',
         );
+        setShowCheck((prevShowCheck) => ({
+          ...prevShowCheck,
+          [Object.keys(data)[0]]: true,
+        }));
         return;
       }
       if (res.status === StatusCodes.BAD_REQUEST) {
@@ -403,25 +410,11 @@ export default function PatientRegistration() {
    * @param {string} value
    */
   async function handleAccordionChange(value) {
-    value === null
-      ? navigate('', { replace: true })
-      : navigate(`#${value}`, { replace: true });
-
-    setVisitedSections((prevVisitedSections) => ({
-      ...prevVisitedSections,
-      [value]: true,
-    }));
-
-    if (!openedSection) {
-      setOpenedSection(value);
-      setActive(TABS.indexOf(value));
+    if (value === null) {
+      navigate('', { replace: true });
       return;
-    }
-
-    if (TABS.indexOf(value) < active) {
-      setOpenedSection(value);
-      setActive(TABS.indexOf(value));
-      return;
+    } else {
+      navigate(`#${value}`, { replace: true });
     }
 
     let errorFieldCount = 0;
@@ -437,7 +430,11 @@ export default function PatientRegistration() {
 
     if (errorFieldCount === 0) {
       setOpenedSection(value);
-      setActive(TABS.indexOf(value));
+
+      setVisitedSections((prevVisitedSections) => ({
+        ...prevVisitedSections,
+        [value]: true,
+      }));
 
       if (openedSection === 'patientData') {
         const patientData = {
@@ -479,6 +476,7 @@ export default function PatientRegistration() {
               initialHospitalData={initialHospitalData}
               initialPhysicianData={initialPhysicianData}
               openedSection={openedSection}
+              showCheck={showCheck}
               handleAccordionChange={handleAccordionChange}
             />
             <Flex justify="center" mt="md">
