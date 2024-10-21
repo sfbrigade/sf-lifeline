2037243bbff7f3e8dbabd13a77c46e365cd4e5d5
samau3 <samau415@gmail.com>
2024-10-20 22:39:36 -0700

    Basic display of all patients in a table format

--- client/src/pages/patients/Patients.jsx
+++ client/src/pages/patients/Patients.jsx
@@ -1,7 +1,84 @@
+import { Table } from '@mantine/core';
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import { StatusCodes } from 'http-status-codes';
+
+import LifelineAPI from './LifelineAPI';
+
+/**
+ *  Patients page component
+ *
+ */
 export default function Patients() {
+  const [search] = useState('');
+  const [page] = useState(1);
+
+  const { data: patients } = useQuery({
+    queryKey: ['patients', search, page],
+    queryFn: async () => {
+      const res = await LifelineAPI.getPatients(search, page);
+      if (res.status === StatusCodes.OK) {
+        return await res.json();
+      } else {
+        throw new Error('Failed to fetch patients.');
+      }
+    },
+  });
+
   return (
     <div>
       <h1>Patients</h1>
+
+      <Table stickyHeader>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th>Name</Table.Th>
+            <Table.Th>Created by</Table.Th>
+            <Table.Th>Date created</Table.Th>
+            <Table.Th>Updated By</Table.Th>
+            <Table.Th>Last updated</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>
+          {patients?.map((patient) => (
+            <Table.Tr key={patient.id}>
+              <Table.Td>
+                {patient.firstName}{' '}
+                {patient.middleName ? patient.middleName + ' ' : ''}
+                {patient.lastName}
+              </Table.Td>
+              <Table.Td>
+                {patient.createdBy.firstName}{' '}
+                {patient.createdBy.middleName
+                  ? patient.createdBy.middleName + ' '
+                  : ''}
+                {patient.createdBy.lastName}
+              </Table.Td>
+              <Table.Td>
+                {new Date(patient.createdAt).toLocaleDateString(undefined, {
+                  month: 'long',
+                  day: 'numeric',
+                  year: 'numeric',
+                })}
+              </Table.Td>
+              <Table.Td>
+                {patient.createdBy.firstName}{' '}
+                {patient.createdBy.middleName
+                  ? patient.createdBy.middleName + ' '
+                  : ''}
+                {patient.createdBy.lastName}
+              </Table.Td>
+              <Table.Td>
+                {new Date(patient.updatedAt).toLocaleDateString(undefined, {
+                  month: 'long',
+                  day: 'numeric',
+                  year: 'numeric',
+                })}
+              </Table.Td>
+            </Table.Tr>
+          ))}
+        </Table.Tbody>
+      </Table>
     </div>
   );
 }
