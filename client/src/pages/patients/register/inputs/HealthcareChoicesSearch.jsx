09401f1ecdd8a0379f51ccc691e3c45468186f64
samau3 <samau415@gmail.com>
2024-10-21 14:01:42 -0700

    Modify string creation logic to remove extra characters

--- client/src/pages/patients/register/inputs/HealthcareChoicesSearch.jsx
+++ client/src/pages/patients/register/inputs/HealthcareChoicesSearch.jsx
@@ -71,15 +71,14 @@ export default function HealthcareChoicesSearch({ form, choice, initialData }) {
 
   const handleSelectValue = (id, key) => {
     const name = key.children;
-    // setValue({ id, name });
-    setSearch(name);
+    setSearch(name.join(""));
     form.setFieldValue(`healthcareChoices.${choice}Id`, id);
     combobox.closeDropdown();
   };
 
   const options = (data || []).map((item) => (
     <Combobox.Option value={item.id} key={item.id}>
-      {item.name}
+      {item.name}{item.hospital ? ` - ${item.hospital}` : ''}
     </Combobox.Option>
   ));
 
