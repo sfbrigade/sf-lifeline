d53b17695a533bc66eaa52077c50b5f5428435dd
samau3 <samau415@gmail.com>
2024-10-20 21:55:32 -0700

    Destructure options so include can be used with function
    The option include causes an error if passed as an argument for count() so it needs to be extracted from options

--- server/prisma/client.js
+++ server/prisma/client.js
@@ -6,12 +6,17 @@ const prisma = new PrismaClient({
   name: 'paginate',
   model: {
     $allModels: {
-      async paginate({ page, perPage, ...options }) {
+      async paginate({ page, perPage, include, ...options }) {
         const take = parseInt(perPage, 10);
         const skip = (parseInt(page, 10) - 1) * take;
         const context = Prisma.getExtensionContext(this);
         const total = await context.count(options);
-        const records = await context.findMany({ ...options, skip, take });
+        const records = await context.findMany({
+          ...options,
+          include,
+          skip,
+          take,
+        });
         return { records, total };
       },
     },
