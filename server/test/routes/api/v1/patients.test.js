f83f1233c1d1b979493ac61e8bf61beb2e1ae7a5
samau3 <samau415@gmail.com>
2024-10-20 20:33:43 -0700

    Add tests for listing patients route

--- server/test/routes/api/v1/patients.test.js
+++ server/test/routes/api/v1/patients.test.js
@@ -5,6 +5,102 @@ import { StatusCodes } from 'http-status-codes';
 import { build } from '../../../helper.js';
 
 describe('/api/v1/patients', () => {
+  describe('GET /', () => {
+    it('should return all patients for ADMIN user', async (t) => {
+      const app = await build(t);
+      await t.loadFixtures();
+      const headers = await t.authenticate('admin.user@test.com', 'test');
+      const reply = await app
+        .inject()
+        .get('/api/v1/patients?patient=&perPage=1')
+        .headers(headers);
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
+      assert.deepStrictEqual(
+        reply.headers['link'],
+        '<http://localhost/api/v1/patients?patient=&perPage=1&page=2>; rel="next",<http://localhost/api/v1/patients?patient=&perPage=1&page=3>; rel="last"',
+      );
+      assert.deepStrictEqual(JSON.parse(reply.payload).length, 1);
+    });
+
+    it('should return patients for STAFF user', async (t) => {
+      const app = await build(t);
+      await t.loadFixtures();
+      const headers = await t.authenticate('staff.user@test.com', 'test');
+      const reply = await app
+        .inject()
+        .get('/api/v1/patients?patient=')
+        .headers(headers);
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
+      assert.deepStrictEqual(JSON.parse(reply.payload).length, 3);
+    });
+
+    it('should return patients for VOLUNTEER user', async (t) => {
+      const app = await build(t);
+      await t.loadFixtures();
+      const headers = await t.authenticate('volunteer.user@test.com', 'test');
+      const reply = await app
+        .inject()
+        .get('/api/v1/patients?patient=')
+        .headers(headers);
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
+      assert.deepStrictEqual(JSON.parse(reply.payload).length, 3);
+    });
+
+    it('should return no patients for FIRST_RESPONDER user', async (t) => {
+      const app = await build(t);
+      await t.loadFixtures();
+      const headers = await t.authenticate('first.responder@test.com', 'test');
+      const reply = await app
+        .inject()
+        .get('/api/v1/patients?patient=')
+        .headers(headers);
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.FORBIDDEN);
+    });
+
+    it('should return a patient for ADMIN user', async (t) => {
+      const app = await build(t);
+      await t.loadFixtures();
+      const headers = await t.authenticate('admin.user@test.com', 'test');
+      const reply = await app
+        .inject()
+        .get('/api/v1/patients?patient=doe')
+        .headers(headers);
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
+      assert.deepStrictEqual(JSON.parse(reply.payload).length, 2);
+      assert.deepStrictEqual(JSON.parse(reply.payload)[0].firstName, 'Jackson');
+      assert.deepStrictEqual(JSON.parse(reply.payload)[1].firstName, 'John');
+    });
+
+    it('should return a patient from querying for their first, last, and full name', async (t) => {
+      const app = await build(t);
+      await t.loadFixtures();
+      const headers = await t.authenticate('admin.user@test.com', 'test');
+      const reply = await app
+        .inject()
+        .get('/api/v1/patients?patient=john')
+        .headers(headers);
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
+      assert.deepStrictEqual(JSON.parse(reply.payload).length, 1);
+      assert.deepStrictEqual(JSON.parse(reply.payload)[0].firstName, 'John');
+
+      const reply2 = await app
+        .inject()
+        .get('/api/v1/patients?patient=john doe')
+        .headers(headers);
+
+      assert.deepStrictEqual(reply2.statusCode, StatusCodes.OK);
+      assert.deepStrictEqual(JSON.parse(reply2.payload).length, 1);
+      assert.deepStrictEqual(JSON.parse(reply2.payload)[0].firstName, 'John');
+      assert.deepStrictEqual(JSON.parse(reply2.payload)[0].lastName, 'Doe');
+    });
+  });
+
   describe('GET /generate', () => {
     it('returns a list of new patient URLs', async (t) => {
       const app = await build(t);
