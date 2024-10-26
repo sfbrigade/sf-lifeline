826e1216c9573805a0cfd1d75dd32b5174b4f7d1
samau3 <samau415@gmail.com>
2024-10-25 23:43:30 -0700

    Add a modal that confirms patient deletion if ADMIN user

--- client/src/pages/patients/PatientsTable.jsx
+++ client/src/pages/patients/PatientsTable.jsx
@@ -1,14 +1,17 @@
 import PropTypes from 'prop-types';
 
-import { Paper, Table, ActionIcon, Menu } from '@mantine/core';
+import { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { Paper, Table, ActionIcon, Menu, Modal, Button } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import {
   IconDotsVertical,
   IconUser,
   IconQrcode,
   IconTrash,
 } from '@tabler/icons-react';
-import { Link } from 'react-router-dom';
 import classes from './Patients.module.css';
+import Context from '../../Context';
 
 const patientTableProps = {
   headers: PropTypes.arrayOf(
@@ -34,69 +37,104 @@ const patientTableProps = {
  * @param {PropTypes.InferProps<typeof patientTableProps>} props
  */
 export default function PatientsTable({ headers, data }) {
+  const [opened, { open, close }] = useDisclosure(false);
+  const [selectedPatient, setSelectedPatient] = useState(null);
+  const { user } = useContext(Context);
+
+  const showDeleteConfirmation = (patient) => {
+    setSelectedPatient(patient);
+    open();
+  };
+
+  const confirmPatientDeletion = () => {
+    console.log('Delete Patient', selectedPatient.id);
+    close();
+  };
+
   return (
-    <Paper withBorder className={classes.tableWrapper}>
-      <Table.ScrollContainer minWidth={500} type="native">
-        <Table
-          stickyHeader
-          highlightOnHover
-          verticalSpacing="lg"
-          classNames={{ table: classes.table }}
-        >
-          <Table.Thead>
-            <Table.Tr>
-              {headers.map((header) => (
-                <Table.Th key={header.key}>{header.text}</Table.Th>
-              ))}
-            </Table.Tr>
-          </Table.Thead>
-          <Table.Tbody>
-            {data.length > 0 ? (
-              data?.map((patient) => (
-                <Table.Tr key={patient.id}>
-                  {headers.map((header) => (
-                    <Table.Td key={patient[header.key] + header.key}>
-                      {patient[header.key]}
+    <>
+      <Paper withBorder className={classes.tableWrapper}>
+        <Table.ScrollContainer minWidth={500} type="native">
+          <Table
+            stickyHeader
+            highlightOnHover
+            verticalSpacing="lg"
+            classNames={{ table: classes.table }}
+          >
+            <Table.Thead>
+              <Table.Tr>
+                {headers.map((header) => (
+                  <Table.Th key={header.key}>{header.text}</Table.Th>
+                ))}
+              </Table.Tr>
+            </Table.Thead>
+            <Table.Tbody>
+              {data.length > 0 ? (
+                data?.map((patient) => (
+                  <Table.Tr key={patient.id}>
+                    {headers.map((header) => (
+                      <Table.Td key={patient[header.key] + header.key}>
+                        {patient[header.key]}
+                      </Table.Td>
+                    ))}
+                    <Table.Td>
+                      <Menu shadow="md">
+                        <Menu.Target>
+                          <ActionIcon variant="subtle" color="gray">
+                            <IconDotsVertical size={18} />
+                          </ActionIcon>
+                        </Menu.Target>
+                        <Menu.Dropdown>
+                          <Menu.Item
+                            leftSection={<IconUser size={18} />}
+                            component={Link}
+                            to={`/patients/${patient.id}`}
+                          >
+                            View/Edit
+                          </Menu.Item>
+                          <Menu.Item leftSection={<IconQrcode size={18} />}>
+                            Reprint QR Code
+                          </Menu.Item>
+                          {user?.role === 'ADMIN' && (
+                            <Menu.Item
+                              leftSection={<IconTrash size={18} />}
+                              color="red"
+                              onClick={() =>
+                                showDeleteConfirmation({
+                                  id: patient.id,
+                                  name: patient.name,
+                                })
+                              }
+                            >
+                              Delete
+                            </Menu.Item>
+                          )}
+                        </Menu.Dropdown>
+                      </Menu>
                     </Table.Td>
-                  ))}
-                  <Table.Td>
-                    <Menu shadow="md">
-                      <Menu.Target>
-                        <ActionIcon variant="subtle" color="gray">
-                          <IconDotsVertical size={18} />
-                        </ActionIcon>
-                      </Menu.Target>
-                      <Menu.Dropdown>
-                        <Menu.Item
-                          leftSection={<IconUser size={18} />}
-                          component={Link}
-                          to={`/patients/${patient.id}`}
-                        >
-                          View/Edit
-                        </Menu.Item>
-                        <Menu.Item leftSection={<IconQrcode size={18} />}>
-                          Reprint QR Code
-                        </Menu.Item>
-                        <Menu.Item
-                          leftSection={<IconTrash size={18} />}
-                          color="red"
-                        >
-                          Delete
-                        </Menu.Item>
-                      </Menu.Dropdown>
-                    </Menu>
+                  </Table.Tr>
+                ))
+              ) : (
+                <Table.Tr>
+                  <Table.Td colSpan={headers.length}>
+                    No patients found.
                   </Table.Td>
                 </Table.Tr>
-              ))
-            ) : (
-              <Table.Tr>
-                <Table.Td colSpan={headers.length}>No patients found.</Table.Td>
-              </Table.Tr>
-            )}
-          </Table.Tbody>
-        </Table>
-      </Table.ScrollContainer>
-    </Paper>
+              )}
+            </Table.Tbody>
+          </Table>
+        </Table.ScrollContainer>
+      </Paper>
+      <Modal opened={opened} onClose={close} title="Delete Patient">
+        <p>Are you sure you want to delete this patient record?</p>
+        <Button color="red" fullWidth onClick={confirmPatientDeletion}>
+          Yes
+        </Button>
+        <Button color="blue" fullWidth onClick={close}>
+          No
+        </Button>
+      </Modal>
+    </>
   );
 }
 
