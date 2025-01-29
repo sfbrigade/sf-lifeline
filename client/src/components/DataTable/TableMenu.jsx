cde5e1fbf1638c7412dbec61b0233c0beb4c5ffd
samau3 <samau415@gmail.com>
2025-01-28 21:53:10 -0800

    Create a reusable menu button based on patient table

---
+++ client/src/components/DataTable/TableMenu.jsx
@@ -0,0 +1,58 @@
+import PropTypes from 'prop-types';
+import { Menu, ActionIcon } from '@mantine/core';
+import { TbDotsVertical as IconDotsVertical } from 'react-icons/tb';
+
+const tableMenuProps = {
+  menuItems: PropTypes.arrayOf(
+    PropTypes.shape({
+      icon: PropTypes.node.isRequired,
+      label: PropTypes.string.isRequired,
+      onClick: PropTypes.func,
+      to: PropTypes.string,
+      color: PropTypes.string,
+      component: PropTypes.elementType,
+      divider: PropTypes.bool,
+      isLabel: PropTypes.bool,
+    })
+  ).isRequired,
+};
+
+/**
+ * Reusable table menu component
+ * @param {PropTypes.InferProps<typeof tableMenuProps>} props
+ */
+export default function TableMenu({ menuItems }) {
+  return (
+    <Menu shadow="md">
+      <Menu.Target>
+        <ActionIcon variant="subtle" color="gray">
+          <IconDotsVertical size={18} />
+        </ActionIcon>
+      </Menu.Target>
+      <Menu.Dropdown>
+        {menuItems.map((item, index) => {
+          if (item.divider) {
+            return <Menu.Divider key={`divider-${index}`} />;
+          }
+          if (item.isLabel) {
+            return <Menu.Label key={`label-${index}`}>{item.label}</Menu.Label>;
+          }
+          return (
+            <Menu.Item
+              key={item.label}
+              leftSection={item.icon}
+              onClick={item.onClick}
+              to={item.to}
+              color={item.color}
+              component={item.component}
+            >
+              {item.label}
+            </Menu.Item>
+          );
+        })}
+      </Menu.Dropdown>
+    </Menu>
+  );
+}
+
+TableMenu.propTypes = tableMenuProps; 
