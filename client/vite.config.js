d8505b9d72a588b9975f08b4474e983b71e4558a
Francis Li <francisli@users.noreply.github.com>
2025-01-28 16:01:08 -0800

    [Closes #191] Patient form UI tweaks (#192)
    * Allow all hosts to connect to vite dev server
    
    * Fix typo in filename
    
    * [Closes #191] Tweak Patient form UI

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: true,
     proxy: {
       '/api': {
         target: 'http://0.0.0.0:5100',
