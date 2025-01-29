569bf3800aecd7f8ad1636389cb0875fd4e2cb9a
Francis Li <mail@francisli.com>
2025-01-28 18:26:00 -0800

    Set default values for form

--- client/src/pages/patients/register/PatientRegistration.jsx
+++ client/src/pages/patients/register/PatientRegistration.jsx
@@ -76,8 +76,8 @@ export default function PatientRegistration () {
         firstName: '',
         middleName: '',
         lastName: '',
-        gender: null,
-        language: null,
+        gender: 'UNKNOWN',
+        language: 'ENGLISH',
         dateOfBirth: '',
       },
       contactData: {
@@ -86,7 +86,7 @@ export default function PatientRegistration () {
         lastName: '',
         phone: '',
         email: '',
-        relationship: null,
+        relationship: 'UNKNOWN',
       },
       medicalData: {
         allergies: [],
@@ -97,7 +97,7 @@ export default function PatientRegistration () {
         hospitalId: '',
         physicianId: '',
       },
-      codeStatus: null,
+      codeStatus: 'FULL',
     },
 
     validate: {
