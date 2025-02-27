11c695ee7b7f4fb03da558035743bb57d445fe3e
Francis Li <francisli@users.noreply.github.com>
2025-02-26 20:52:52 -0800

    [Closes #210] Fix client build, add client build as part of CI to prevent unexpected failures (#211)
    * Fix broken react-router imports
    
    * Add client build as part of CI
    
    * Update dependencies, fix prisma studio launch

--- client/src/stories/AuthLayout/AuthLayout.jsx
+++ client/src/stories/AuthLayout/AuthLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router';
 import { Image, Text, Title } from '@mantine/core';
 
 import classes from './AuthLayout.module.css';
