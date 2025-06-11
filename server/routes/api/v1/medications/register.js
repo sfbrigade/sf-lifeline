82b160ee7d62f08297c3c341264ae75a9d0d476d
Saman Givian <samangivian@Samans-MacBook-Pro.local>
2025-06-11 07:50:08 -0700

    238 - test coverage

--- server/routes/api/v1/medications/register.js
+++ server/routes/api/v1/medications/register.js
@@ -20,6 +20,7 @@ export default async function (fastify) {
               name: { type: 'string' },
               system: { type: 'string' },
               code: { type: 'string' },
+              altNames: { type: 'string' },
             },
           },
         },
