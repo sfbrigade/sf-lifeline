bacfe1eefc613f4bd469795ee5d795377673f5a6
Saman Givian <samangivian@Samans-MacBook-Pro.local>
2025-06-11 09:35:53 -0700

    238 - test case updated

--- server/test/routes/api/v1/allergies.test.js
+++ server/test/routes/api/v1/allergies.test.js
@@ -1,4 +1,4 @@
-import { beforeEach, describe, it } from 'node:test';
+import { describe, it } from 'node:test';
 import * as assert from 'node:assert';
 import { build } from '../../../helper.js';
 import { StatusCodes } from 'http-status-codes';
--- server/test/routes/api/v1/medications.test.js
+++ server/test/routes/api/v1/medications.test.js
@@ -1,3 +1,4 @@
+import { describe, it } from 'node:test';
 import * as assert from 'node:assert';
 import { build } from '../../../helper.js';
 import { StatusCodes } from 'http-status-codes';
