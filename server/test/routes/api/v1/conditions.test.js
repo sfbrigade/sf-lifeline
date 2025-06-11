75a175690731948c8bcc92ec571dd59e58ed17a4
Saman Givian <samangivian@Samans-MacBook-Pro.local>
2025-06-10 19:13:45 -0700

    238 - Added test coverage

--- server/test/routes/api/v1/conditions.test.js
+++ server/test/routes/api/v1/conditions.test.js
@@ -87,4 +87,120 @@ describe('/api/v1/conditions', () => {
       assert.deepStrictEqual(JSON.parse(reply.payload).message, 'Name cannot be empty or just spaces.');
     });
   });
+
+  describe('GET /', () => {
+    it('should return valid results for admin user', async () => {
+      const reply = await app
+        .inject()
+        .get('/api/v1/conditions?condition=a&perPage=1')
+        .headers(headers);
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
+      assert.deepStrictEqual(
+        reply.headers['link'],
+        '<http://localhost/api/v1/conditions?condition=a&perPage=1&page=2>; rel="next"'
+      );
+      assert.deepStrictEqual(JSON.parse(reply.payload), [
+        {
+          id: '00000000-0000-0000-0000-000000000001',
+          name: 'Asthma',
+          category: 'Respiratory',
+          system: 'ICD10',
+          code: 'J45.909',
+        },
+      ]);
+    });
+
+    it('should return valid results for staff user', async (t) => {
+      const staffHeaders = await t.authenticate('staff.user@test.com', 'test');
+
+      const reply = await app
+        .inject()
+        .get('/api/v1/conditions?condition=a&perPage=1')
+        .headers(staffHeaders);
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
+      assert.deepStrictEqual(
+        reply.headers['link'],
+        '<http://localhost/api/v1/conditions?condition=a&perPage=1&page=2>; rel="next"'
+      );
+      assert.deepStrictEqual(JSON.parse(reply.payload), [
+        {
+          id: '00000000-0000-0000-0000-000000000001',
+          name: 'Asthma',
+          category: 'Respiratory',
+          system: 'ICD10',
+          code: 'J45.909',
+        },
+      ]);
+    });
+
+    it('should return valid results for volunteer user', async (t) => {
+      const volunteerHeaders = await t.authenticate(
+        'volunteer.user@test.com',
+        'test'
+      );
+
+      const reply = await app
+        .inject()
+        .get('/api/v1/conditions?condition=a&perPage=1')
+        .headers(volunteerHeaders);
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
+      assert.deepStrictEqual(
+        reply.headers['link'],
+        '<http://localhost/api/v1/conditions?condition=a&perPage=1&page=2>; rel="next"'
+      );
+      assert.deepStrictEqual(JSON.parse(reply.payload), [
+        {
+          id: '00000000-0000-0000-0000-000000000001',
+          name: 'Asthma',
+          category: 'Respiratory',
+          system: 'ICD10',
+          code: 'J45.909',
+        },
+      ]);
+    });
+
+    it('require a user to be admin/staff/volunteer to make requests', async () => {
+      const reply = await app
+        .inject()
+        .get('/api/v1/conditions?condition=a&perPage=1');
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.UNAUTHORIZED);
+    });
+
+    it('should return paginated results of all conditions when no query provided', async () => {
+      const reply = await app
+        .inject()
+        .get('/api/v1/conditions?condition=&perPage=1')
+        .headers(headers);
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
+      assert.deepStrictEqual(
+        reply.headers['link'],
+        '<http://localhost/api/v1/conditions?condition=&perPage=1&page=2>; rel="next",<http://localhost/api/v1/conditions?condition=&perPage=1&page=3>; rel="last"'
+      );
+      assert.deepStrictEqual(JSON.parse(reply.payload), [
+        {
+          id: '00000000-0000-0000-0000-000000000001',
+          name: 'Asthma',
+          category: 'Respiratory',
+          system: 'ICD10',
+          code: 'J45.909',
+        },
+      ]);
+    });
+
+    it('should return no results from database for an unknown condition', async () => {
+      const reply = await app
+        .inject()
+        .get('/api/v1/conditions?condition=newcondition&perPage=1')
+        .headers(headers);
+
+      assert.deepStrictEqual(reply.statusCode, StatusCodes.OK);
+      assert.deepStrictEqual(reply.headers['link'], '');
+      assert.deepStrictEqual(JSON.parse(reply.payload), []);
+    });
+  });
 });
