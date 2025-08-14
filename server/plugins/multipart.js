389d344df5ac6ca611e4856601f3947f54cda5e2
Saman Givian <samangivian@Samans-MacBook-Pro.local>
2025-08-14 11:04:44 -0700

    changed file sizing

--- server/plugins/multipart.js
+++ server/plugins/multipart.js
@@ -4,5 +4,6 @@ import multipart from '@fastify/multipart';
 export default fp(async function (fastify) {
   fastify.register(multipart, {
     attachFieldsToBody: true,
+    limits: { fileSize: 5 * 1024 * 1024 }
   });
 });
